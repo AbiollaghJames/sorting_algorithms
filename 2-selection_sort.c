f28de33c42d9c1fff48679db34a1b5453d116114
Nataly-Aketch <nataliebeckie@gmail.com>
2023-04-21 13:49:52 +0300

    Bug fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,12 +9,10 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-<<<<<<< HEAD
 	if (size > 2)
 	{
 		/* Interate the array 1 index at a time */
 		for (i = 0; i < size - 1; i++)
-=======
 	/* Interate the array 1 index at a time */
 	for (i = 0; i < size - 1; i++)
 	{
@@ -22,7 +20,6 @@ void selection_sort(int *array, size_t size)
 
 		/* find index of min number in unsorted array */
 		for (j = i + 1; j < size; j++)
->>>>>>> eaca11305a037f6fbb5e2a540a5f61bd655b9c20
 		{
 			size_t min_val = i;
 
@@ -45,18 +42,5 @@ void selection_sort(int *array, size_t size)
 			}
 			print_array(array, size);
 		}
-<<<<<<< HEAD
-=======
-		/* check equality of min_value index and i index then swap */
-		if (min_val != i)
-		{
-			size_t temp;
-
-			temp = array[i];
-			array[i] = array[min_val];
-			array[min_val] = temp;
-			print_array(array, size);
-		}
->>>>>>> eaca11305a037f6fbb5e2a540a5f61bd655b9c20
 	}
 }
