98defa1a1d8f6380a8303b553b5ba9ae81dd5631
AbiollaghJames <abiollajames@protonmail.com>
2023-04-21 04:19:00 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,31 +9,28 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	if (size > 2)
+	/* Interate the array 1 index at a time */
+	for (i = 0; i < size - 1; i++)
 	{
-		/* Interate the array 1 index at a time */
-		for (i = 0; i < size - 1; i++)
+		size_t min_val = i;
+
+		/* find index of min number in unsorted array */
+		for (j = i + 1; j < size; j++)
 		{
-			size_t min_val = i;
-
-			/* find index of min number in unsorted array */
-			for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_val])
 			{
-				if (array[j] < array[min_val])
-				{
-					min_val = j;
-				}
+				min_val = j;
 			}
-			/* check equality of min_value index and i index then swap */
-			if (min_val != i)
-			{
-				size_t temp;
+		}
+		/* check equality of min_value index and i index then swap */
+		if (min_val != i)
+		{
+			size_t temp;
 
-				temp = array[i];
-				array[i] = array[min_val];
-				array[min_val] = temp;
-				print_array(array, size);
-			}
+			temp = array[i];
+			array[i] = array[min_val];
+			array[min_val] = temp;
+			print_array(array, size);
 		}
 	}
 }
