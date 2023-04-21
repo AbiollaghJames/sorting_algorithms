b726309fed7b2a7d333ebaeae5e18bb28f4e0525
Nataly-Aketch <nataliebeckie@gmail.com>
2023-04-21 13:45:17 +0300

    Bug fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,39 @@
 #include "sort.h"
 /**
+ * swap - swaps two ints
+ * @x: first int
+ * @y: second int
+ */
+void swap(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+/**
  * bubble_sort - sorts an array using bubble sort
  * @array: array to be sort
  * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp;
+	size_t i, j;
 
-	/*accesses all array elements*/
-	for (i = 0; i < size - 1; i++)
+	if (size > 2)
 	{
-		/*compares all array elements*/
-		for (j = 0; j < size - 1; j++)
+		/*accesses all array elements*/
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			/*compares all array elements*/
+			for (j = 0; j < size - 1; j++)
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+				}
 			}
 		}
 	}
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,28 +7,32 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	
-	/* Interate the array 1 index at a time */
-	for (i = 0; i < size - 1; i++)
+
+	if (size > 2)
 	{
-		size_t min_val = i;
-		
-		/* find index of min number in unsorted array */
-		for (j = i + 1; j < size; j++)
+		/* Interate the array 1 index at a time */
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] < array[min_val])
+			size_t min_val = i;
+
+			/* find index of min number in unsorted array */
+			for (j = i + 1; j < size; j++)
 			{
-				min_val = j;
+				if (array[j] < array[min_val])
+				{
+					min_val = j;
+				}
 			}
+			/* check equality of min_value index and i index then swap */
+			if (min_val != i)
+			{
+				size_t temp;
+
+				temp = array[i];
+				array[i] = array[min_val];
+				array[min_val] = temp;
+			}
+			print_array(array, size);
 		}
-		/* check equality of min_value index and i index then swap */
-		if (min_val != i)
-		{
-			size_t temp;
-			temp = array[i];
-			array[i] = array[min_val];
-			array[min_val] = temp;
-		}
-		print_array(array, size);
 	}
 }
