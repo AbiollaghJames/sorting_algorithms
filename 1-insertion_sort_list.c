60af3dfcefa52d47cf579fa130d0377c2741eb4c
AbiollaghJames <abiollajames@protonmail.com>
2023-04-22 21:22:51 -0700

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+/*
+ * insertion_sort - function that implements insertion sort
+ * @list: doubly linked list to perform sorting on
+ * Return: Nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr, *prev, *nxt;
+
+	/* if list is empty or has one node, its sorted */
+	if (list == NULL || (*list)->next == NULL)
+		return;
+
+	/* Traverse list from next or second node */
+	curr = (*list)->next;
+
+	while (curr != NULL)
+	{
+		nxt = curr->next;
+		prev = curr->prev;
+
+		while (prev != NULL && prev->n > curr->n)
+		{
+			prev->next = curr->next;
+
+			if (curr->next != NULL)
+				curr->next->prev = prev;
+			curr->next = prev;
+			curr->prev = prev->prev;
+			prev->prev = curr;
+
+			if (curr->prev == NULL)
+				*list = curr;
+			else
+				curr->prev->next = curr;
+			print_list(*list);
+			prev = curr->prev;
+		}
+		curr = nxt;
+	}
+}
